c05990eeafe2b9629717581f290e1466e1dbba8e
Richard <josevd83@hotmail.com>
2016-09-15 08:55:00 -0400

    subiendo el ejercicio donde se imprime la tabla de multiplicar (clase 1)

--- javaOperaciones/src/javaoperaciones/JavaOperaciones.java
+++ javaOperaciones/src/javaoperaciones/JavaOperaciones.java
@@ -16,6 +16,15 @@ public class JavaOperaciones {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        for(int i = 0; i<=10; i++){
+            System.out.println("\n********************************* " + i);
+            System.out.println("\nTABLA DE MULTIPLICAR DEL "+ i);
+            
+            for(int j = 0; j<=10; j++ ){
+                System.out.println(i + " x " + j + " = " + i*j);
+            }
+        }
+        
     }
     
 }
