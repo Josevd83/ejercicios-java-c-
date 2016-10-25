6174f24a0d5f64f010c527b4e1b72f5908713ad1
Richard <josevd83@hotmail.com>
2016-10-25 07:33:21 -0400

    Subiendo video 20: numeros capicuas

---
+++ javaNumerosCapicuas/src/javanumeroscapicuas/JavaNumerosCapicuas.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javanumeroscapicuas;
+
+/**
+ *
+ * @author rsilva
+ */
+public class JavaNumerosCapicuas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+        /*EJEMPLO EN C++*/
+        /*
+        int main(int argc, char** argv){
+        //strlen  longitud
+        //strcpy  copiar cadenas
+        //strrev  al reves
+        //strcmp  comparar
+        
+        char numero[]="1234321";
+        char aux[strlen(numero)];
+        strcpy(aux, numero);
+        
+        if(strcmp(aux, strrev(numero))==0){
+            cout << aux << "es capicua";
+        }
+        else{
+            cout << aux << " no es capicua";
+        }
+            
+        }*/
+    }
+    
+    
+}
