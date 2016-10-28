4c1ca4fbf6f9c72f5a23ff46e73fb86aea5c076e
Richard <josevd83@hotmail.com>
2016-10-28 07:01:06 -0400

    subiendo video 22: intercambio de pares de caracteres

---
+++ javaIntercamabiarParesDeStrings/src/javaintercamabiarparesdestrings/JavaIntercamabiarParesDeStrings.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaintercamabiarparesdestrings;
+
+/**
+ *
+ * @author rsilva
+ */
+public class JavaIntercamabiarParesDeStrings {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*
+        char cadena[] = "Esta frase va a ser alterada";
+        char aux;
+        
+        for(int i=0; i<strlen(cadena); i++){
+            if((i+1) < strlen(cadena)){
+                aux=cadena[i];
+                cadena[i]=cadena[i+1];
+                cadena[i+1]=aux;
+                i++;
+            }
+        }
+        
+        cout<< cadena;
+        */
+    }
+    
+}
