cf49ebda0182c50b1ffbf2cc37de93d57e274870
Richard <josevd83@hotmail.com>
2016-11-01 07:11:56 -0400

    Subiendo video 23: quitando espacios en blanco en una cadena

---
+++ javaQuitarEspaciosDeUnaCadena/src/javaquitarespaciosdeunacadena/JavaQuitarEspaciosDeUnaCadena.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaquitarespaciosdeunacadena;
+
+/**
+ *
+ * @author rsilva
+ */
+public class JavaQuitarEspaciosDeUnaCadena {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        /*cogigo c++
+        char cadena[] = "a         esta frase le vamos a quitar los espacios";
+        int espacio;
+        int aux;
+        
+        for(int i=0;i<strlen(cadena); i++){
+            if(cadena[i]==32){
+                espacio=i;
+                aux=i;
+                while(cadena[aux]==32 && aux<strlen(cadena)-1) aux++;
+                cadena[espacio]=cadena[aux];
+                cadena[aux] = 32;
+            }
+        }
+        cout<<cadena;
+        */
+    }
+    
+}
